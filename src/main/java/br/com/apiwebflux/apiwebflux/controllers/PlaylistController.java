f97e0337950edd8e1abf1a26d33485ceafba68fa
RickFerry <rick_ferry1@hotmail.com>
2023-06-01 23:32:16 -0300

    CRUD

--- src/main/java/br/com/apiwebflux/apiwebflux/controllers/PlaylistController.java
+++ src/main/java/br/com/apiwebflux/apiwebflux/controllers/PlaylistController.java
@@ -2,6 +2,9 @@ package br.com.apiwebflux.apiwebflux.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,16 +12,27 @@ import br.com.apiwebflux.apiwebflux.models.Playlist;
 import br.com.apiwebflux.apiwebflux.services.PlaylistService;
 import lombok.AllArgsConstructor;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/playlists")
 public class PlaylistController {
-    
+
     private PlaylistService playlistService;
 
     @GetMapping
     public ResponseEntity<Flux<Playlist>> findAll() {
         return ResponseEntity.ok(playlistService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Mono<Playlist>> findById(@PathVariable String id) {
+        return ResponseEntity.ok(playlistService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Mono<Playlist>> save(@RequestBody Playlist p) {
+        return ResponseEntity.ok(playlistService.save(p));
     }
 }
--- src/main/java/br/com/apiwebflux/apiwebflux/services/PlaylistService.java
+++ src/main/java/br/com/apiwebflux/apiwebflux/services/PlaylistService.java
@@ -6,14 +6,23 @@ import br.com.apiwebflux.apiwebflux.models.Playlist;
 import br.com.apiwebflux.apiwebflux.repositorys.PlaylistRepository;
 import lombok.AllArgsConstructor;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 @AllArgsConstructor
 public class PlaylistService {
-    
+
     private PlaylistRepository playlistRepository;
 
     public Flux<Playlist> findAll() {
         return playlistRepository.findAll();
+    }
+
+    public Mono<Playlist> findById(String id) {
+        return playlistRepository.findById(id);
+    }
+
+    public Mono<Playlist> save(Playlist p) {
+        return this.playlistRepository.save(p);
     }
 }
